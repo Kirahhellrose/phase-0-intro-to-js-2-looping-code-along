604eec47a5085160b79d321c044eeb5151b4641c
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-05-30 11:11:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+//wrapGifts(gifts);
+
+function writeCards(names, event) {
+  const thanks = [];
+  for (let i = 0; i < names.length; i++) {
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thanks;
+}
+function countDown(num) {
+  while (num >= 0) {
+      console.log(num);
+      num--;
+  }
+}
